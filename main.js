547d61b61f3e15b29c35258f48a4e2827462dbd8
Abhyudit Pandey <114844250+AbhyuditPandey@users.noreply.github.com>
2023-04-04 17:52:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function start()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/-rvpPGOY1/model.json',modelready);
+    }
+
+    function modelready()
+    {
+        classifier.classify(gotresult);
+        
+    }
+
+//https://teachablemachine.withgoogle.com/models/-rvpPGOY1/
